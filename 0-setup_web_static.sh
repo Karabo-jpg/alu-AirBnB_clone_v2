fe93474ee489cdd407ddfc07259576c10588d2ed
karabo-jpg <k.ojiambo@alustudent.com>
2025-06-01 14:03:41 +0300

    Add setup script for web_static deployment

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+# Script that sets up web servers for the deployment of web_static
+
+# Install Nginx if not already installed
+apt-get update
+apt-get -y install nginx
+
+# Create necessary directories if they don't exist
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+
+# Create fake HTML file
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+
+# Create or recreate symbolic link
+rm -rf /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership to ubuntu user and group
+chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+config_string="server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By \$hostname;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current/;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 https://github.com/Karabo-jpg;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+        root /var/www/html;
+        internal;
+    }
+}"
+
+echo "$config_string" > /etc/nginx/sites-available/default
+
+# Restart Nginx
+service nginx restart
+
+exit 0 
