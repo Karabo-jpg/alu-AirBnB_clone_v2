1f43bbbf445f5174abb5c0aca05a59465f687832
karabo-jpg <k.ojiambo@alustudent.com>
2025-06-01 15:30:54 +0300

    Fix script exit code and remove sudo commands

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 # Script that sets up your web servers for the deployment of web_static
 
+# Exit on error
+set -e
 
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo service nginx start
+apt-get -y update
+apt-get -y install nginx
+service nginx start
 
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu:ubuntu /data/
+chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
-sudo service nginx restart 
+service nginx restart
+
+exit 0 
