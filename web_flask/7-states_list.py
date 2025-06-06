d59675423e1ddebbec9e1a21a6a253961c670d64
karabo-jpg <k.ojiambo@alustudent.com>
2025-06-06 18:13:44 +0300

    Task 8: Add states list page with exact formatting

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,6 +3,7 @@
 Script that starts a Flask web application:
 - Your web application must be listening on 0.0.0.0, port 5000
 - You must use storage for fetching data from the storage engine
+- You must use storage.all() to fetch States
 """
 from flask import Flask, render_template
 from models import storage
@@ -20,10 +21,10 @@ def teardown_db(exception):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Display a HTML page with the list of all State objects.
-    States are sorted by name.
+    States are sorted by name (A->Z).
     """
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda x: x.name)
     return render_template('7-states_list.html', states=sorted_states)
 
 
