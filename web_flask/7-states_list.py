cb83bbba32cdc8a05bc72a956e4108cf56ddc267
karabo-jpg <k.ojiambo@alustudent.com>
2025-06-06 16:15:57 +0300

    Task 8: Add states list display with Flask

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application:
+- Your web application must be listening on 0.0.0.0, port 5000
+- You must use storage for fetching data from the storage engine
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a HTML page with the list of all State objects.
+    States are sorted by name.
+    """
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000) 
