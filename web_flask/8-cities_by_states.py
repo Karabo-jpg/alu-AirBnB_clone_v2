a8efaad06ac2cbdd556c5fa15c1e4cf4fe136de7
karabo-jpg <k.ojiambo@alustudent.com>
2025-06-08 12:22:07 +0300

    Added Task 9: Cities by states - Flask route and template

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application:
+- Your web application must be listening on 0.0.0.0, port 5000
+- You must use storage for fetching data from the storage engine
+- After each request you must remove the current SQLAlchemy Session
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close storage"""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page with a list of all states and their cities"""
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda x: x.name)
+    return render_template('8-cities_by_states.html', states=sorted_states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000) 
